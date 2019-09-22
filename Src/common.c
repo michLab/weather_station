f94d371d7c53fa7f0c8dd67ea0cc22997a8df399
Michal Labowski <michal.labowski@gmail.com>
2019-09-22 20:53:07 +0200

    feat: Add comments to common.c

--- Src/common.c
+++ Src/common.c
@@ -1,11 +1,18 @@
 /*
- * common.c
+ * \file common.c
+ * \brief Common functions definitions
  *
- *  Created on: Sep 22, 2019
- *      Author: michal
+ * \author	Michal Labowski
  */
+
 #include <common.h>
 
+/*
+ * \fn console_show
+ * \brief Puts data to the serial console
+ * \param[in] huart	A pointer to the UART handle (HAL)
+ * \param[in] data A pointer to data to show
+ */
 int console_show(UART_HandleTypeDef *huart, char* data) {
 	char uart_tx_data[300];
 	int uart_tx_data_size = sprintf(uart_tx_data, "%s\n\r", data);
